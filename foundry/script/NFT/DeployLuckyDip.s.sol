4d6cd9d76acd70aab36483edb7e7886425889f61
pascalthaochanta <pascal.thao-chanta@precom.fr>
2024-11-06 22:33:06 +0100

    added svg conversation to script

--- foundry/script/NFT/DeployLuckyDip.s.sol
+++ foundry/script/NFT/DeployLuckyDip.s.sol
@@ -4,16 +4,26 @@ pragma solidity 0.8.20;
 import {Script, console} from "forge-std/Script.sol";
 import {NFTLuckyDip} from "../../src/NFT/NFTLuckyDip.sol";
 import {LuckyDip, NFTSet} from "../../src/NFT/structs/LuckyDip.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract DeployNFTLuckyDip is Script {
+    string constant SVG_PATH = "./images/dynamicNft/";
     string[] s_tmpPaths;
     LuckyDip[] s_luckyDips;
 
     function run() external returns (NFTLuckyDip luckyDip) {
-        populateLuckyDips();
         vm.startBroadcast();
         luckyDip = new NFTLuckyDip();
-        s_tmpPaths = ["image1", "image2"];
+        vm.stopBroadcast();
+
+        s_tmpPaths = [
+            svgToImageURI(
+                vm.readFile(string(abi.encodePacked(SVG_PATH, "image1")))
+            ),
+            svgToImageURI(
+                vm.readFile(string(abi.encodePacked(SVG_PATH, "image2")))
+            )
+        ];
         luckyDip.addLuckyDip(
             "a nice colletion from JeanMichelJarre",
             "JMJ",
@@ -23,37 +33,35 @@ contract DeployNFTLuckyDip is Script {
             0,
             s_tmpPaths
         );
-        vm.stopBroadcast();
-    }
-
-    function populateLuckyDips() internal {
-        populateLuckyDip(
-            "a nice colletion from JeanMichelJarre",
-            "JMJ",
-            "Jeanmi",
-            1e16,
-            1e15,
-            0
-        );
-        s_tmpPaths = ["imlage3", "image4"];
-        populateLuckyDip(
+        s_tmpPaths = [
+            svgToImageURI(
+                vm.readFile(string(abi.encodePacked(SVG_PATH, "image3")))
+            ),
+            svgToImageURI(
+                vm.readFile(string(abi.encodePacked(SVG_PATH, "image4")))
+            )
+        ];
+        luckyDip.addLuckyDip(
             "a nice colletion from Zidane",
             "ZZ",
             "Zizou",
             2e16,
             2e15,
-            1
+            1,
+            s_tmpPaths
         );
+
+        return luckyDip;
     }
 
-    function populateLuckyDip(
-        string memory _description,
-        string memory _symbol,
-        string memory _name,
-        uint256 _startingBid,
-        uint256 _bidStep,
-        uint256 index
-    ) internal {
-        // TODO ! https://ethereum.stackexchange.com/questions/85374/type-struct-mycontract-user-memory-is-not-implicitly-convertible-to-expected-typ
+    // You could also just upload the raw SVG and have solildity convert it!
+    function svgToImageURI(
+        string memory svg
+    ) public pure returns (string memory) {
+        string memory baseURI = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(
+            bytes(string(abi.encodePacked(svg))) // Removing unnecessary type castings, this line can be resumed as follows : 'abi.encodePacked(svg)'
+        );
+        return string(abi.encodePacked(baseURI, svgBase64Encoded));
     }
 }
