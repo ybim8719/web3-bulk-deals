f8b00994457de91bccdf4c12eb11dd0b0f943383
pascalthaochanta <pascal.thao-chanta@precom.fr>
2024-11-11 22:52:44 +0100

    new instructions and useless code removed

--- foundry/src/NFT/NFTLuckyDip.sol
+++ foundry/src/NFT/NFTLuckyDip.sol
@@ -8,6 +8,14 @@ import {LuckyDip} from "./structs/LuckyDip.sol";
 // 1) add an oracle triggering service to execute automatically closure of luckyDipBidden depending on validity time
 // 2) rethink about payable membership...
 
+/**
+ * @title NFTLuckyDip is a bid application of luckyDips selling the ownership of a set of nft designed by a unique artist
+ * The artworks are hosted on chain, since they are  svg files encoded on base 64. The particylarity of the lukcy dips is that the
+ * pictures are only exposed when the final bid is achieved and final NFT contract deployed.
+ * @author ybim
+ * @notice After deployment of contract, a unlimited number of lucky dips can be send by the owner of the contract
+ * For more, follow instructions in the read me file or in the Contract script/DeployNFTLuckyDip.s.sol
+ */
 contract NFTLuckyDip {
     /** * ERRORS */
     error NFTLuckyDip__OwnerOnly();
@@ -43,13 +51,6 @@ contract NFTLuckyDip {
         _;
     }
 
-    modifier memberOnly() {
-        if (s_members[msg.sender] == false) {
-            revert NFTLuckyDip__MemberOnly();
-        }
-        _;
-    }
-
     constructor() {
         i_owner = msg.sender;
     }
@@ -80,44 +81,7 @@ contract NFTLuckyDip {
         );
     }
 
-    function applyForMembership() public payable {
-        if (msg.value != MEMBERSHIP_FEE) {
-            revert NFTLuckyDip__InvalidMembershipFeeSent({
-                required: MEMBERSHIP_FEE,
-                passed: msg.value
-            });
-        }
-        if (s_members[msg.sender] == true) {
-            revert NFTLuckyDip__ApplierAlreadyRegistered({applier: msg.sender});
-        }
-        s_members[msg.sender] = true;
-    }
-
-    function removeMembership(address memberToRemove) public ownerOnly {
-        if (msg.sender == memberToRemove) {
-            revert NFTLuckyDip__CantRemoveOwnerFromMembers();
-        }
-        if (s_members[memberToRemove] == false) {
-            revert NFTLuckyDip__InexistantMember(memberToRemove);
-        }
-        // TODO member has bought a collectionNFT
-        // if (s_pendingProposals[memberToRemove].length > 0) {
-        //     revert NFTLuckyDip__MemberHasPendingProposal(memberToRemove);
-        // }
-
-        // refund membership fee if contract funds are ok
-        if (address(this).balance > MEMBERSHIP_FEE) {
-            revert NFTLuckyDip__UnsufficientFunds(address(this).balance);
-        }
-        // refund the initial fee to the member to remove
-        (bool callSuccess, ) = payable(memberToRemove).call{
-            value: MEMBERSHIP_FEE
-        }("");
-        require(callSuccess, "Call failed");
-        s_members[memberToRemove] = false;
-    }
-
-    function bidForLuckyDip(uint256 i) public payable memberOnly {
+    function bidForLuckyDip(uint256 i) public payable {
         //check if amount sent is ok for winning
         // cehck conract balance and send back the money to previous bidder.
         // update the luckydip info with the new bestBidder address, and the next step to reach
